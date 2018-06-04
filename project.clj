e90400ddedcbf916fa6b9d9a2fb74cbf04ac3410
Derek Troy-West <derek@troywest.com>
2018-06-04 14:22:25 +1000

    project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject com.troy-west/cronut "0.1.0-SNAPSHOT"
+
+  :description "Scheduled Execution via Quartzite and Integrant"
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [integrant "0.6.3"]]
+
+  :pedantic? :abort)
