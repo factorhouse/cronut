364a8b926064427efef78359e163337929477342
Derek Troy-West <derek@troywest.com>
2018-06-08 12:16:00 +1000

    simple and cron trigger support + shortcut data readers

--- src/data_readers.clj
+++ src/data_readers.clj
@@ -1,1 +1,3 @@
-{cronut/trigger troy-west.cronut/trigger}
+{cronut/trigger  troy-west.cronut/trigger
+ cronut/cron     troy-west.cronut/shortcut-cron
+ cronut/interval troy-west.cronut/shortcut-interval}
--- src/troy_west/cronut.clj
+++ src/troy_west/cronut.clj
@@ -3,12 +3,13 @@
   (:require [clojure.edn :as edn]
             [clojure.tools.logging :as log]
             [integrant.core :as ig])
-  (:import (org.quartz Scheduler Job SimpleScheduleBuilder JobExecutionException JobBuilder TriggerBuilder JobDetail)
+  (:import (org.quartz Scheduler Job SimpleScheduleBuilder JobExecutionException JobBuilder TriggerBuilder JobDetail CronScheduleBuilder)
            (org.quartz.impl StdSchedulerFactory)
            (org.quartz.spi JobFactory TriggerFiredBundle)
            (java.util TimeZone)))
 
-(defn trigger-builder
+(defn base-trigger-builder
+  "Provide a base trigger-builder from configuration"
   [{:keys [identity description start end priority]}]
   (cond-> (TriggerBuilder/newTrigger)
     (seq identity) (.withIdentity (first identity) (second identity))
@@ -19,6 +20,7 @@
     priority (.withPriority (int priority))))
 
 (defn simple-schedule
+  "Provide a simple schedule from configuration"
   [{:keys [interval time-unit repeat misfire]}]
   (let [schedule (SimpleScheduleBuilder/simpleSchedule)]
     (case time-unit
@@ -40,13 +42,30 @@
       (= :forever repeat) (.repeatForever schedule))
     schedule))
 
-(defmulti trigger :type)
+(defn cron-schedule
+  "Provide a cron schedule from configuration"
+  [{:keys [cron time-zone misfire]}]
+  (let [schedule (CronScheduleBuilder/cronSchedule ^String cron)]
+    (case misfire
+      :ignore (.withMisfireHandlingInstructionIgnoreMisfires schedule)
+      :do-nothing (.withMisfireHandlingInstructionDoNothing schedule)
+      :fire-and-proceed (.withMisfireHandlingInstructionFireAndProceed schedule)
+      nil nil)
+    (when time-zone
+      (.inTimeZone schedule (TimeZone/getTimeZone ^String time-zone)))
+    schedule))
 
-(defmethod trigger :simple
+(defmulti trigger-builder :type)
+
+(defmethod trigger-builder :simple
   [config]
-  (-> (trigger-builder config)
-      (.withSchedule (simple-schedule config))
-      (.build)))
+  (-> (base-trigger-builder config)
+      (.withSchedule (simple-schedule config))))
+
+(defmethod trigger-builder :cron
+  [config]
+  (-> (base-trigger-builder config)
+      (.withSchedule (cron-schedule config))))
 
 (defrecord ProxyJob [proxied-job]
   Job
@@ -77,17 +96,22 @@
               (boolean? durable?) (.storeDurably durable?)))))
 
 (defn activate
-  [scheduler schedule]
-  ;; TODO: potentially improve this loop to carry job->proxy-job map and re-use in the case of job
-  ;; TODO: with multiple triggers, currently we create multiple proxy-jobs
-  ;; TODO: actually thinking about it we'd need to support that due to re-used jobs with name clashes
+  [^Scheduler scheduler schedule]
+  (.clear scheduler)
   (loop [schedule  schedule
-         scheduled {}]
-    (if-let [{:keys [job trigger]} (first schedule)]
-      (let [proxy-detail (proxy job)]
-        (log/info "scheduling" trigger proxy-detail)
-        (.scheduleJob scheduler proxy-detail trigger)
-        (recur (rest schedule) (assoc scheduled (.getKey ^JobDetail proxy-detail) job)))
+         scheduled {}
+         proxies   {}]
+    (if-let [{:keys [job ^TriggerBuilder trigger]} (first schedule)]
+      (if-let [previously-scheduled ^JobDetail (get proxies job)]
+        (let [built (.build (.forJob trigger previously-scheduled))]
+          (log/info "scheduling new trigger for existing job" trigger previously-scheduled)
+          (.scheduleJob scheduler built)
+          (recur (rest schedule) scheduled proxies))
+        (let [proxy-detail ^JobDetail (proxy job)
+              job-key      (.getKey proxy-detail)]
+          (log/info "scheduling new job" trigger proxy-detail)
+          (.scheduleJob scheduler proxy-detail (.build trigger))
+          (recur (rest schedule) (assoc scheduled job-key job) (assoc proxies job proxy-detail))))
       (.setJobFactory scheduler (job-factory scheduled))))
   (.start scheduler)
   scheduler)
@@ -104,13 +128,26 @@
       (log/infof "with quartz update check disabled" time-zone))
     (activate (StdSchedulerFactory/getDefaultScheduler) schedule)))
 
+(defn shortcut-interval
+  "Trigger immediately, at an interval-ms, run forever (well that's optimistic but you get the idea)"
+  [interval-ms]
+  (trigger-builder {:type      :simple
+                    :interval  interval-ms
+                    :time-unit :millis
+                    :repeat    :forever}))
+
+(defn shortcut-cron
+  [cron]
+  (trigger-builder {:type :cron
+                    :cron cron}))
+
 (defn shutdown
   [scheduler]
   (.shutdown ^Scheduler scheduler))
 
 (defmethod ig/init-key :cronut/trigger
   [_ config]
-  (trigger config))
+  (trigger-builder config))
 
 (defmethod ig/init-key :cronut/scheduler
   [_ config]
@@ -121,10 +158,13 @@
   (shutdown scheduler))
 
 (def data-readers
-  {'ig/ref           ig/ref
-   'cronut/trigger   troy-west.cronut/trigger})
+  {'ig/ref          ig/ref
+   'cronut/trigger  troy-west.cronut/trigger-builder
+   'cronut/cron     troy-west.cronut/shortcut-cron
+   'cronut/interval troy-west.cronut/shortcut-interval})
 
 (defn init-system
+  "Convenience for starting integrant systems with cronut data-readers"
   ([config]
    (init-system config nil))
   ([config readers]
