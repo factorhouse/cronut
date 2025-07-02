22dde29d1140bd27d8e31549f0e329fa832c599b
Derek Troy-West <derek@factorhouse.io>
2025-07-02 14:57:03 +1000

    bump ci and deps

--- project.clj
+++ project.clj
@@ -7,17 +7,17 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v20.html"}
 
-  :plugins [[lein-cljfmt "0.9.0" :exclusions [org.clojure/clojure]]]
+  :plugins [[dev.weavejester/lein-cljfmt "0.13.1"]]
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [org.quartz-scheduler/quartz "2.3.2" :exclusions [org.slf4j/slf4j-api]]
-                 [integrant "0.8.0" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [org.quartz-scheduler/quartz "2.4.0" :exclusions [org.slf4j/slf4j-api]]
+                 [integrant "0.13.1" :scope "provided"]]
 
   :profiles {:dev {:resource-paths ["test-resources"]
-                   :dependencies   [[ch.qos.logback/logback-classic "1.2.11"]
-                                    [org.clojure/core.async "1.5.648"]
-                                    [clj-kondo "2022.09.08"]]}}
+                   :dependencies   [[ch.qos.logback/logback-classic "1.3.15"]
+                                    [org.clojure/core.async "1.8.741"]
+                                    [clj-kondo "2025.06.05"]]}}
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
