13c49d8f0ac916d151be148962ac9c53eadbcca9
Derek Troy-West <derek@factorhouse.io>
2025-07-22 00:12:35 +1000

    rm integrant dependency from core cronut projects

--- cronut-javax/project.clj
+++ cronut-javax/project.clj
@@ -11,8 +11,7 @@
 
   :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/tools.logging "1.3.0"]
-                 [org.quartz-scheduler/quartz "2.4.0" :exclusions [org.slf4j/slf4j-api]]
-                 [integrant "0.13.1" :scope "provided"]]
+                 [org.quartz-scheduler/quartz "2.4.0" :exclusions [org.slf4j/slf4j-api]]]
 
   :profiles {:dev   {:resource-paths ["dev-resources"]
                      :dependencies   [[ch.qos.logback/logback-classic "1.3.15"]
--- cronut/src/data_readers.clj
+++
@@ -1,3 +0,0 @@
-{cronut/trigger  cronut/trigger-builder
- cronut/cron     cronut/shortcut-cron
- cronut/interval cronut/shortcut-interval}
