d72d2e67185d232c97a8c02d88caefe371c2534d
Derek Troy-West <derek@factorhouse.io>
2025-07-22 00:00:20 +1000

    clojure-integrant javax/jakarta profiles

--- cronut-integrant/project.clj
+++ cronut-integrant/project.clj
@@ -12,18 +12,19 @@
   :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/tools.logging "1.3.0"]]
 
-  :profiles {:dev   {:resource-paths ["dev-resources"]
-                     :dependencies   [[io.factorhouse/cronut "0.2.7"]
-                                      [integrant "0.13.1"]
-                                      [ch.qos.logback/logback-classic "1.5.18"]
-                                      [org.slf4j/slf4j-api "2.0.17"]
-                                      [org.clojure/core.async "1.8.741"]
-                                      [clj-kondo "2025.06.05"]]}
-             :smoke {:pedantic? :abort}}
+  :profiles {:dev     {:resource-paths ["dev-resources"]
+                       :dependencies   [[integrant "0.13.1"]
+                                        [ch.qos.logback/logback-classic "1.5.18"]
+                                        [org.slf4j/slf4j-api "2.0.17"]
+                                        [org.clojure/core.async "1.8.741"]
+                                        [clj-kondo "2025.06.05"]]}
+             :jakarta {:dependencies [[io.factorhouse/cronut "0.2.7"]]} ;; TODO fix up after release
+             :javax   {:dependencies [[io.factorhouse/cronut "0.2.7"]]} ;; TODO fix up after release
+             :smoke   {:pedantic? :abort}}
 
-  :aliases {"check" ["with-profile" "+smoke" "check"]
-            "kondo" ["with-profile" "+smoke" "run" "-m" "clj-kondo.main" "--lint" "src:src:test:test" "--parallel"]
-            "fmt"   ["with-profile" "+smoke" "cljfmt" "check"]}
+  :aliases {"check" ["with-profile" "+smoke,+jakarta" "check"]
+            "kondo" ["with-profile" "+smoke,+jakarta" "run" "-m" "clj-kondo.main" "--lint" "src:src:test:test" "--parallel"]
+            "fmt"   ["with-profile" "+smoke,+jakarta" "cljfmt" "check"]}
 
   :source-paths ["src"]
   :test-paths ["test"]
