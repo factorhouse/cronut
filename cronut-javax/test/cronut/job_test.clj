c96f37670557ab177e6cf3ba33928fda90bf5f57
Derek Troy-West <derek@factorhouse.io>
2025-07-23 17:26:07 +1000

    fix spelling

--- cronut-javax/test/cronut/job_test.clj
+++ cronut-javax/test/cronut/job_test.clj
@@ -8,7 +8,9 @@
                :jobClass
                :description
                :durable
-               :concurrentExecutionDisallowed
+               ;; note: this spelling mistake remains in 2.4.0, fixed in 2.5.0
+               ;; https://github.com/quartz-scheduler/quartz/pull/1165
+               :concurrentExectionDisallowed
                :jobDataMap])
 
 (def reify-job (reify Job
@@ -16,16 +18,16 @@
 
 (deftest job-detail-meta
 
-  (is (= {:fullName                      "group2.name1"
-          :jobClass                      SerialProxyJob
-          :description                   "desc3"
-          :durable                       true
-          :concurrentExecutionDisallowed true
-          :jobDataMap                    {"job-impl" {:description                    "desc3"
-                                                      :disallow-concurrent-execution? true
-                                                      :durable?                       true
-                                                      :identity                       ["name1" "group2"]
-                                                      :recover?                       true}}}
+  (is (= {:fullName                     "group2.name1"
+          :jobClass                     SerialProxyJob
+          :description                  "desc3"
+          :durable                      true
+          :concurrentExectionDisallowed true
+          :jobDataMap                   {"job-impl" {:description                    "desc3"
+                                                     :disallow-concurrent-execution? true
+                                                     :durable?                       true
+                                                     :identity                       ["name1" "group2"]
+                                                     :recover?                       true}}}
          (-> (job/detail {:identity                       ["name1" "group2"]
                           :description                    "desc3"
                           :durable?                       true
@@ -40,7 +42,7 @@
   (is (= {:jobClass                      ProxyJob
           :description                   nil
           :durable                       false
-          :concurrentExecutionDisallowed false
+          :concurrentExectionDisallowed false
           :jobDataMap                    {"job-impl" reify-job}}
          (-> (job/detail reify-job false)
              (bean)
@@ -51,7 +53,7 @@
   (is (= {:jobClass                      SerialProxyJob
           :description                   nil
           :durable                       false
-          :concurrentExecutionDisallowed true
+          :concurrentExectionDisallowed true
           :jobDataMap                    {"job-impl" reify-job}}
          (-> (job/detail reify-job true)
              (bean)
@@ -65,7 +67,7 @@
   (is (= {:jobClass                      ProxyJob
           :description                   nil
           :durable                       false
-          :concurrentExecutionDisallowed false
+          :concurrentExectionDisallowed false
           :jobDataMap                    {"job-impl" {}}}
          (-> (job/detail {} false)
              (bean)
@@ -77,7 +79,7 @@
   (is (= {:jobClass                      SerialProxyJob
           :description                   nil
           :durable                       false
-          :concurrentExecutionDisallowed true
+          :concurrentExectionDisallowed true
           :jobDataMap                    {"job-impl" {}}}
          (-> (job/detail {} true)
              (bean)
@@ -89,7 +91,7 @@
   (is (= {:jobClass                      SerialProxyJob
           :description                   nil
           :durable                       false
-          :concurrentExecutionDisallowed true
+          :concurrentExectionDisallowed true
           :jobDataMap                    {"job-impl" {:disallow-concurrent-execution? true}}}
          (-> (job/detail {:disallow-concurrent-execution? true} false)
              (bean)
@@ -101,7 +103,7 @@
   (is (= {:jobClass                      SerialProxyJob
           :description                   nil
           :durable                       false
-          :concurrentExecutionDisallowed true
+          :concurrentExectionDisallowed true
           :jobDataMap                    {"job-impl" {:disallow-concurrent-execution? true}}}
          (-> (job/detail {:disallow-concurrent-execution? true} false)
              (bean)
