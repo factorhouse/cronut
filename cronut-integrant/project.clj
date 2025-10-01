bdb8920a26981522befbf1522510f877e0e900d8
Derek Troy-West <derek@factorhouse.io>
2025-10-01 14:39:52 +1000

    fix pedantic and update cljfmt

--- cronut-integrant/project.clj
+++ cronut-integrant/project.clj
@@ -7,7 +7,7 @@
   :license {:name "Apache 2.0 License"
             :url  "https://github.com/factorhosue/slipway/blob/main/LICENSE"}
 
-  :plugins [[dev.weavejester/lein-cljfmt "0.13.1"]]
+  :plugins [[dev.weavejester/lein-cljfmt "0.13.4"]]
 
   :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/tools.logging "1.3.0"]]
@@ -17,7 +17,7 @@
                                         [ch.qos.logback/logback-classic "1.5.19"]
                                         [org.slf4j/slf4j-api "2.0.17"]
                                         [org.clojure/core.async "1.8.741"]
-                                        [clj-kondo "2025.09.22"]]}
+                                        [clj-kondo "2025.09.22" :exclusions [org.clojure/tools.reader]]]}
              :jakarta {:dependencies [[io.factorhouse/cronut "1.0.0"]]}
              :javax   {:dependencies [[io.factorhouse/cronut-javax "1.0.0"]]}
              :smoke   {:pedantic? :abort}}
