05723ba6c1be76a9d8ce48ceeb70807c566b2180
Derek Troy-West <derek@factorhouse.io>
2025-10-01 14:31:41 +1000

    update deps

--- cronut-integrant/project.clj
+++ cronut-integrant/project.clj
@@ -9,15 +9,15 @@
 
   :plugins [[dev.weavejester/lein-cljfmt "0.13.1"]]
 
-  :dependencies [[org.clojure/clojure "1.12.1"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/tools.logging "1.3.0"]]
 
   :profiles {:dev     {:resource-paths ["dev-resources"]
                        :dependencies   [[integrant "0.13.1"]
-                                        [ch.qos.logback/logback-classic "1.5.18"]
+                                        [ch.qos.logback/logback-classic "1.5.19"]
                                         [org.slf4j/slf4j-api "2.0.17"]
                                         [org.clojure/core.async "1.8.741"]
-                                        [clj-kondo "2025.06.05"]]}
+                                        [clj-kondo "2025.09.22"]]}
              :jakarta {:dependencies [[io.factorhouse/cronut "1.0.0"]]}
              :javax   {:dependencies [[io.factorhouse/cronut-javax "1.0.0"]]}
              :smoke   {:pedantic? :abort}}
--- cronut-javax/project.clj
+++ cronut-javax/project.clj
@@ -9,7 +9,7 @@
 
   :plugins [[dev.weavejester/lein-cljfmt "0.13.1"]]
 
-  :dependencies [[org.clojure/clojure "1.12.1"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/tools.logging "1.3.0"]
                  [org.quartz-scheduler/quartz "2.4.0" :exclusions [org.slf4j/slf4j-api]]]
 
@@ -17,7 +17,7 @@
                      :dependencies   [[ch.qos.logback/logback-classic "1.3.15"]
                                       [org.slf4j/slf4j-api "2.0.17"]
                                       [org.clojure/core.async "1.8.741"]
-                                      [clj-kondo "2025.06.05"]]}
+                                      [clj-kondo "2025.09.22"]]}
              :smoke {:pedantic? :abort}}
 
   :aliases {"check"  ["with-profile" "+smoke" "check"]
