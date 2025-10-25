ee887143c0a52c7547d612e22c75035f5f739543
Derek Troy-West <derek@factorhouse.io>
2025-10-25 15:57:58 -0700

    update dependencies

--- project.clj
+++ project.clj
@@ -7,17 +7,17 @@
   :license {:name "Apache 2.0 License"
             :url  "https://github.com/factorhosue/slipway/blob/main/LICENSE"}
 
-  :plugins [[dev.weavejester/lein-cljfmt "0.13.4"]]
+  :plugins [[dev.weavejester/lein-cljfmt "0.15.3"]]
 
   :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/tools.logging "1.3.0"]
-                 [org.quartz-scheduler/quartz "2.5.0" :exclusions [org.slf4j/slf4j-api]]]
+                 [org.quartz-scheduler/quartz "2.5.1" :exclusions [org.slf4j/slf4j-api]]]
 
   :profiles {:dev   {:resource-paths ["dev-resources"]
-                     :dependencies   [[ch.qos.logback/logback-classic "1.5.19"]
+                     :dependencies   [[ch.qos.logback/logback-classic "1.5.20"]
                                       [org.slf4j/slf4j-api "2.0.17"]
                                       [org.clojure/core.async "1.8.741"]
-                                      [clj-kondo "2025.09.22" :exclusions [org.clojure/tools.reader]]]}
+                                      [clj-kondo "2025.10.23" :exclusions [org.clojure/tools.reader]]]}
              :smoke {:pedantic? :abort}}
 
   :aliases {"check"  ["with-profile" "+smoke" "check"]
